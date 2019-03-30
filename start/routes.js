50e73fe2980250fcfdb47b8e827e129ee702246e
zoe606 <rive99erlangga@gmail.com>
2019-03-30 12:05:27 +0700

    initial project

---
+++ start/routes.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('welcome')
+
+Route.get('/tes', () => 'Hello World')
+
+Route.get('/there', function() {
+  return 'Hello There';
+})
+
+// ES6 Method
+Route.get('/there/:id', function({ params }) {
+  return `This is the id ${params.id}`;
+})
+
+Route.on('/home').render('home')
