f9f6020cc1f1c77f773841c92823b408917cc5a3
zoe606 <rive99erlangga@gmail.com>
2019-03-30 23:23:56 +0700

    edit and delete

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -58,6 +58,51 @@ class PostController {
 
     return response.redirect('/posts')
   }
+
+  async edit({ params, view })
+  {
+    const post = await Post.find(params.id)
+
+    return view.render('posts.edit', {
+      post: post
+    })
+  }
+
+  async update({ params, request, response, session})
+  {
+    // validation input
+    const validation = await validate(request.all(), {
+      title: 'required|min:3|max:125',
+      body: 'required|min:3'
+    })
+
+    if(validation.fails()){
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+
+    const post = await Post.find(params.id)
+
+    post.title = request.input('title')
+    post.body = request.input('body')
+
+    await post.save()
+
+    session.flash({ notification: 'Post updated!'})
+
+    return response.redirect('/posts')
+  }
+
+  async destroy({ params, session, response})
+  {
+    const post = await Post.find(params.id)
+
+    await post.delete()
+
+    session.flash({ notification: 'Post Deleted!'})
+
+    return response.redirect('/posts')
+  }
 }
 
 module.exports = PostController
--- start/routes.js
+++ start/routes.js
@@ -33,5 +33,8 @@ Route.on('/home').render('home')
 
 Route.get('/posts', 'PostController.index')
 Route.get('/posts/add', 'PostController.add')
+Route.get('/posts/edit/:id', 'PostController.edit')
 Route.get('/posts/:id', 'PostController.details')
 Route.post('/posts', 'PostController.store')
+Route.put('/posts/:id', 'PostController.update')
+Route.delete('/posts/:id', 'PostController.destroy')
